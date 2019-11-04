26a0dce82b6499136799b729cacf040c8b350171
ryu-g <em.ryu.g@gmail.com>
2019-11-04 21:44:18 +0900

    Make command-wait loop

--- client/index.ts
+++ client/index.ts
@@ -1,4 +1,11 @@
 import * as readlineSync from 'readline-sync'
 
-const name = readlineSync.question('hogehoge?:')
-console.log(`your name is ${name}`)
+const name = readlineSync.question('what your name?:')
+console.log(`hello, ${name}! glhf!`)
+
+while( true ){
+
+  const action = readlineSync.question(`${name}> `)
+  console.log(`*[SYSTEM] ${action} starting...`)//メソッドとかが入る
+
+}
