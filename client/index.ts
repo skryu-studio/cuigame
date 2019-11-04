f0350f57fc651a1b99af7e1d54f7217d2442e0b8
ryu-g <em.ryu.g@gmail.com>
2019-11-04 22:05:29 +0900

    Add method to judge command

--- client/index.ts
+++ client/index.ts
@@ -3,9 +3,25 @@ import * as readlineSync from 'readline-sync'
 const name = readlineSync.question('what your name?:')
 console.log(`hello, ${name}! glhf!`)
 
+const judgeCommand = (action) =>{
+  if(action == ''){//空行のとき
+    return
+  }
+  switch (action) {
+    case "namakocanon":
+      console.log(`*[SYSTEM] ${action} starting...`)
+      break
+    
+    default:
+      console.log(`*[SYSTEM] unknown command "${action}"`)
+      break
+  }
+}
+
+
 while( true ){
 
   const action = readlineSync.question(`${name}> `)
-  console.log(`*[SYSTEM] ${action} starting...`)//メソッドとかが入る
-
+  judgeCommand(action)
 }
+
