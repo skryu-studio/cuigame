89100c0835ea2e2052c9a4868470d732c23c34a8
sKawashima <bysk.333@gmail.com>
2019-11-16 18:25:19 +0900

    📝 Run npx prettier --write index.ts

--- client/index.ts
+++ client/index.ts
@@ -1,18 +1,18 @@
 import * as readlineSync from 'readline-sync'
 
 const map = [
-  [0,0,0,0,0],
-  [0,0,0,0,0],
-  [0,0,0,0,0],
-  [0,0,0,0,0],
-  [0,0,0,0,0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
 ]
 
 const player = {
-  'name': 'playername',
-  'health': 10,
-  'positionX': 2,
-  'positionY': 3
+  name: 'playername',
+  health: 10,
+  positionX: 2,
+  positionY: 3,
 }
 
 const userInit = () => {
@@ -20,45 +20,51 @@ const userInit = () => {
   player.positionX = 1
   player.positionY = 1
   console.log(`*[SYSTEM] hello, ${player.name}! glhf!`)
-  console.log(`*[SYSTEM] now, your position is (${player.positionX}, ${player.positionY}).`)
+  console.log(
+    `*[SYSTEM] now, your position is (${player.positionX}, ${player.positionY}).`
+  )
 }
 
-const move = (directionChain) => {
-  console.log(`*[SYSTEM] ok, move start from (${player.positionX}, ${player.positionY}).`)
+const move = directionChain => {
+  console.log(
+    `*[SYSTEM] ok, move start from (${player.positionX}, ${player.positionY}).`
+  )
   let count = 0
-  while (count < directionChain.length){
-    const direction = directionChain.slice(count , count+1)
+  while (count < directionChain.length) {
+    const direction = directionChain.slice(count, count + 1)
     switch (direction) {
-      case "r":
+      case 'r':
         player.positionX += 1
         break
-      
-      case "l":
+
+      case 'l':
         player.positionX -= 1
         break
-      
-      case "u":
+
+      case 'u':
         player.positionY += 1
         break
-      
-      case "d":
+
+      case 'd':
         player.positionY -= 1
         break
-      
+
       default:
         break
     }
     count++
   }
-  console.log(`*[SYSTEM] now, your position is (${player.positionX}, ${player.positionY}).`)
+  console.log(
+    `*[SYSTEM] now, your position is (${player.positionX}, ${player.positionY}).`
+  )
 }
 
 const shoot = () => {
-  console.log("shoot")
+  console.log('shoot')
 }
 
 const info = () => {
-  console.log(`< your status >`);
+  console.log(`< your status >`)
   console.log(`*[name] : ${player.name}`)
   console.log(`*[health] : ${player.health}`)
   console.log(`*[position] : (${player.positionX},${player.positionY})`)
@@ -66,47 +72,48 @@ const info = () => {
 
 userInit()
 
-const judgeCommand = (action) =>{
-  if(action == ''){//空行のとき
+const judgeCommand = action => {
+  if (action == '') {
+    //空行のとき
     return
   }
   const arg = action.split(' ')
 
   switch (arg[0]) {
     case 'namako':
-      if ( arg.length != 2){
+      if (arg.length != 2) {
         console.log(`*[SYSTEM] (ex) 'namako canon'`)
         break
       }
-      if (arg[1] == 'canon'){
+      if (arg[1] == 'canon') {
         console.log(`*[SYSTEM] ${action} starting...`)
-      }else{
+      } else {
         console.log(`*[SYSTEM] ${arg[1]} is not 'namako' argument`)
       }
       break
 
     case 'survay':
-      if (arg.length != 2){
+      if (arg.length != 2) {
         console.log(`*[SYSTEM] (ex) 'survay [1~2]' to survay`)
         break
       }
-      if (typeof Number(arg[1]) == 'number'){
+      if (typeof Number(arg[1]) == 'number') {
         console.log(`*[SYSTEM] ${action} starting...`)
-      }else{
+      } else {
         console.log(`*[SYSTEM] ${action} is not 'survay' argument`)
       }
       break
 
     case 'move':
-      if(arg.length != 2){
-        console.log("*[SYSTEM] (ex) if you wanna RIGHT 2,UP 1, type")
+      if (arg.length != 2) {
+        console.log('*[SYSTEM] (ex) if you wanna RIGHT 2,UP 1, type')
         console.log("*[SYSTEM]      'move rru' or 'move urr' or 'move rur'")
         break
       }
       move(arg[1])
       break
     case 'info':
-      if(arg.length != 1){
+      if (arg.length != 1) {
         console.log("*[SYSTEM] for your information, just type 'info'.  ")
         break
       }
@@ -114,13 +121,12 @@ const judgeCommand = (action) =>{
       break
 
     case 'shoot':
-      if(arg.length != 1){
-        console.log("*[SYSTEM] just type shoot")
+      if (arg.length != 1) {
+        console.log('*[SYSTEM] just type shoot')
         break
       }
       shoot()
       break
-
 
     default:
       console.log(`*[SYSTEM] unknown command '${action}'`)
@@ -128,7 +134,7 @@ const judgeCommand = (action) =>{
   }
 }
 
-while( true ){
+while (true) {
   const action = readlineSync.question(`${player.name}> `)
   judgeCommand(action)
 }
